c471191ddefefb17904d0770c7b34ea2faf90420
Ivan2604 <ivan.delrr@gmail.com>
2020-02-24 11:55:00 +0100

    fix postgres migration

--- src/main/java/es/api/atw/configuration/ApplicationConfig.java
+++ src/main/java/es/api/atw/configuration/ApplicationConfig.java
@@ -1,8 +1,10 @@
 package es.api.atw.configuration;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(basePackages="es.api.atw.repository")
+@EnableAutoConfiguration
 public class ApplicationConfig {}
